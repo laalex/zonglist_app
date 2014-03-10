8c7c9e8411bfde2e8037c58d24dd34cf476cc3ef
Alexandru Andrei <roshkattu94@gmail.com>
2014-03-10 06:11:23 +0200

    Added splash screen
    - added splash screen
    - modified login screen
    - added styles to it

---
+++ zongList/src/main/java/com/zonglist/SplashScreen.java
@@ -0,0 +1,60 @@
+package com.zonglist;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreen extends ActionBarActivity {
+
+    public static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar bar = getActionBar();
+        bar.hide();
+        setContentView(R.layout.activity_splash_screen);
+        //Show the splash screen
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer.
+             */
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
